a54a59f959c25fd1fffddb9fb7d3e7958a860a4f
ProgrammerAccount <tymek.janiak@wp.pl>
2019-11-03 13:42:37 +0100

    Calendar Events Design

--- Reminder/src/app/calendar/event/event.component.ts
+++ Reminder/src/app/calendar/event/event.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-event',
   templateUrl: './event.component.html',
   styleUrls: ['./event.component.css']
 })
-export class EventComponent implements OnInit {
 
+export class EventComponent implements OnInit {
+  @Input() event: Event;
   constructor() { }
 
   ngOnInit() {
   }
 
+  ClosePopUpMenu(popupmenu:HTMLElement){
+    popupmenu.classList.remove("active");
+    document.getElementsByClassName('overlay')[0].classList.remove('active');
+
+  }
+  OpenPopUpMenu(popupmenu:HTMLElement){
+    popupmenu.classList.add("active");
+    document.getElementsByClassName('overlay')[0].classList.add('active');
+  }
 }
